21215afa738dec49f61f0134d98f1d5e90582f5e
Dave1475 <92506173+Dave1475@users.noreply.github.com>
2025-01-22 10:37:25 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kokoro-onnx-flask",  # Project name
+    version="0.1",             # Version of the project
+    packages=find_packages(),  # Automatically find packages
+    install_requires=[         # List of dependencies
+        "flask",
+        "kokoro-onnx",  # Assuming kokoro-onnx is a separate installable package
+        "numpy",
+        "scipy",
+        "spacy",
+    ],
+    # Optional metadata
+    author="Your Name",
+    description="Flask server wrapper for kokoro-onnx",
+    url="https://github.com/Dave1475/kokoro-onnx-flask",
+)
