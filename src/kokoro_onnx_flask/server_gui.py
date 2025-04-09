470e7e040c43300cdab5b96b67c1004830c33e61
Dave1475 <92506173+Dave1475@users.noreply.github.com>
2025-04-09 11:37:36 -0700

    Update server_gui.py

--- src/kokoro_onnx_flask/server_gui.py
+++ src/kokoro_onnx_flask/server_gui.py
@@ -319,4 +319,4 @@ def generate_and_stream_audio():
     return send_file(audio_stream, mimetype="audio/wav")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8880)
